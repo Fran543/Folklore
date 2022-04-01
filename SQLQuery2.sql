dbcced920a37c57e7a603cdf14b1d76538beed37
init <frandiklic@gmail.com>
2022-04-01 20:19:02 +0200

    Create SQLQuery2.sql

---
+++ SQLQuery2.sql
@@ -0,0 +1,72 @@
+create database MovieDatabase
+go
+
+use MovieDatabase
+go
+
+--USER
+
+create table AppUser(
+	IDUser int primary key identity,
+	Username nvarchar(50),
+	Email nvarchar(50),
+	Password nvarchar(50),
+	Active bit
+)
+go
+
+
+
+--CLIENT CRUD
+
+create proc createUser
+	@Username nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Active bit,
+	@IDUser int output
+as
+begin
+	insert into AppUser (Username, Email, Password, Active) values (@Username, @Email, @Password, @Active)
+	set @IDUser = SCOPE_IDENTITY()
+end
+go
+
+create proc selectUsers 
+as
+begin 
+	select u.IDUser, u.Email, u.Username, u.Password, u.Active
+	from AppUser as u
+end
+go
+
+create proc selectUser
+	@IDUser int
+as
+begin 
+	select u.IDUser, u.Email, u.Username, u.Password, u.Active
+	from AppUser as u
+	where IDUser = @IDUser
+end
+go
+
+create proc updatePassword
+	@Password nvarchar(50),
+	@IDUser int 
+as
+begin
+	update AppUser set
+		Password = @Password
+	where IDUser = @IDUser
+end
+go
+
+create proc deleteUser
+	@IDUser int
+as
+begin
+	update AppUser set
+		Active = 0
+	where IDUser = @IDUser
+end
+go
