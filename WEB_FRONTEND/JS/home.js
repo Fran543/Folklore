d350b3837f5a4c5dee8189514424c8191bea8dcc
Nina <barbaricnina55@gmail.com>
2022-04-04 19:25:53 +0200

    storyCreator

--- WEB_FRONTEND/JS/home.js
+++ WEB_FRONTEND/JS/home.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
         success: function (response) {
             response.forEach(element => {
                 $("#postsContainer").append(
+<<<<<<< Updated upstream
                     "<li>"
                     + "<a class='card'>"
                     + "<img src='" + element.ImageBlob + "' class='card__image' alt=''/>"
@@ -21,6 +22,32 @@ $(document).ready(function () {
                     + "<p class='card__content'>" + element.Content + "</p >"
                     + "</a>"
                     + "</li >"
+=======
+                    "<div class='postCard'>"
+                        + "<p>" + element.PubDate + "</p>"
+                        + "<img src='data:image/png;base64," + element.ImageBlob + "'/>"
+                        + "<h2>" + element.PostName + "</h2>"
+                        + "<div class='warnings'>"
+                            + "<button type=button' class='btn btn-success disabled' disabled>Success</button>"
+                            + "<button type='button' class='btn btn-info disabled' disabled>Info</button>"
+                            + "<button type='button' class='btn btn-warning disabled' disabled>Warning</button>"
+                            + "<button type='button' class='btn btn-danger disabled' disabled>Danger</button>"
+                        + "</div>"
+                        + "<textarea readonly>" + element.Summary + "</textarea>"
+                        + "<div class='btns'>"
+                        +   "<button><i class='fa fa-comment'></i></button>"
+                        +   "<button><i class='fa fa-star'></i></button>"
+                        +   "<button><i class='fa fa-book'></i></button>"
+                        + "</div>"
+                    + "</div>"
+                    //         + "<img src='data:image/png;base64," + element.ImageBlob + "' class='card__image' alt='' />"
+                    //         + "<h3 class='card__title'>" + element.PostName + "</h3>"
+                    //         + "<span class='card__pubDate'>" + element.PubDate + "</span>"
+                    //         + "<span class='card__summary'>" + element.Summary + "</span>"
+                    //         + "<p class='card__content'>" + element.Content + "</p >"
+                    //     + "<div>"
+                    // + "</div >"
+>>>>>>> Stashed changes
                 )
             });
         },
