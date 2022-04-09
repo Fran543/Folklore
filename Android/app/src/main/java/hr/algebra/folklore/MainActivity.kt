fa7d227a4bda4ad17bcfb75dc781cb5fc00c42e8
Leon Kranjčević <lkranj1@racunarstvo.hr>
2022-04-09 12:21:29 +0200

    Prebacivanje sa signupa na login
    dodano sa signupa na login, ali ne obratno jer sa activity-ja na fragment neide to moram još pregledat

--- Android/app/src/main/java/hr/algebra/folklore/MainActivity.kt
+++ Android/app/src/main/java/hr/algebra/folklore/MainActivity.kt
@@ -2,10 +2,26 @@ package hr.algebra.folklore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.algebra.folklore.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners()
     }
+
+    private fun setupListeners() {
+        //TODO
+    }
+
+    /*<action
+    android:id="@+id/mainActivity_to_SignUp"
+    app:destination="@+id/signUpFragment"/>*/
+
 }
--- Android/app/src/main/java/hr/algebra/folklore/SignUpFragment.kt
+++ Android/app/src/main/java/hr/algebra/folklore/SignUpFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import hr.algebra.folklore.databinding.FragmentSignUpBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,11 +23,20 @@ class SignUpFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentSignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    private fun setupListeners() {
+        binding.btnLogIn.setOnClickListener{
+            findNavController().navigate(R.id.action_signUpFragment_to_mainActivity)
         }
     }
 
@@ -33,7 +44,7 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setupListeners()
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
