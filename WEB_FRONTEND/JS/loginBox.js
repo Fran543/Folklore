d720c16e3c1f247d36fe6843eca61aa40d698fae
NinaBarb <61802507+NinaBarb@users.noreply.github.com>
2022-04-23 18:07:01 +0200

    search bar, trending

--- WEB_FRONTEND/JS/loginBox.js
+++ WEB_FRONTEND/JS/loginBox.js
@@ -17,6 +17,7 @@ var registerEndPoint = "http://127.0.0.1:8091/register"
 var loginEndPoint = "http://127.0.0.1:8091/login"
 
 $("#registerForm").submit(function (event) {
+    debugger
     event.preventDefault()
     $.ajax({
         url: registerEndPoint,
